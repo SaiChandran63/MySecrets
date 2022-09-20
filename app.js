b614fed4afb72ab39253285bbc1a0bd556669219
Sai <silentcoder143@gmail.com>
2022-09-20 19:36:57 +0530

    Final stage

--- app.js
+++ app.js
@@ -248,6 +248,11 @@ app.post('/login', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server is up and running on port 3000");
 });
